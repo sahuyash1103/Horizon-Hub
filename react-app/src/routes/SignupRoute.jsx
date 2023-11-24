0e09d2f0f9df67f0f3decc78377b5b38d1ed0739
Imsaymaahmad <sayma902902@gmail.com>
2023-11-24 20:01:12 +0530

    added login and signup ui

--- react-app/src/routes/SignupRoute.jsx
+++ react-app/src/routes/SignupRoute.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
+import "../signup.css"
+import { IoChatbubble } from "react-icons/io5";
+import { SiGmail } from "react-icons/si";
+import { FaLock } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaMobile } from "react-icons/fa6";
 
 import { signup } from '../axios/api/auth/auth.req';
 import { setUser } from '../redux-toolkit/reducers/auth';
+import { useNavigate } from 'react-router-dom';
 
 function SignupRoute() {
     const dispatch = useDispatch();
@@ -20,31 +27,54 @@ function SignupRoute() {
         dispatch(setUser({ user: res.data, token: res.token }))
     }
 
-    return (<div>
-        <h1>Login</h1>
-        <form onSubmit={handleLogin}>
-            <input type="text"
-                placeholder='Enter your name'
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-            />
-            <input type="text"
-                placeholder='Enter your phone number'
-                onChange={(e) => setPhone(e.target.value)}
-                value={phone}
-            />
-            <input type="email"
-                placeholder='Enter your email'
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-            />
-            <input type="password"
-                placeholder='Enter your password'
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
-            <button type="submit" >Login</button>
-        </form>
+    return (<div className='container'>
+        <IoChatbubble className="chat" />
+        <h2 className='h2'>ChitChat</h2>
+        <h3 className='h3'>Connect and Communicate</h3>
+        <div className='form2'>
+            <form onSubmit={handleLogin}>
+                <div className='txt_field'>
+                    <FaUser className='icon' />
+                    <input type="text"
+                        placeholder='Enter your name'
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
+                    <span></span>
+                </div>
+                <div className='txt_field'>
+                    <FaMobile className='icon' />
+                    <input type="text"
+                        placeholder='Enter your phone number'
+                        onChange={(e) => setPhone(e.target.value)}
+                        value={phone}
+                    />
+                    <span></span>
+                </div>
+                <div className='txt_field'>
+                    <SiGmail className='icon' />
+                    <input type="email"
+                        placeholder='Enter your email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                    <span></span>
+                </div>
+                <div className='txt_field'>
+                    <FaLock className="icon" />
+                    <input type="password"
+                        placeholder='Enter your password'
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                    <span></span>
+                </div>
+                <button className='lgn_btn'>SignUp</button>
+                <div class="login_link">Please Click Here to
+                    <a href="./login.html"> Login</a>
+                </div>
+            </form>
+        </div>
     </div>
     );
 }
